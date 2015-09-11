726c0e3b5316a43d80952692b3723d5e3d7a0a2b
Erik <Erik@T61P.home>
2015-09-11 13:47:56 +0200

    Added start message

--- GitDemoProject/src/com/sws/MainClass.java
+++ GitDemoProject/src/com/sws/MainClass.java
@@ -3,6 +3,7 @@ package com.sws;
 public class MainClass {
 	
 	public static void main( String[] args ) {
+		System.out.println("Starting MainClass");
 		PrintToConsole printToConsole = new PrintToConsole();
 		printToConsole.printMeassage();
 	}
